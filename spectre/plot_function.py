d3910cb881480b4532dec495610e0cf1eaeeba4e
PregY <iflp1908sl@gmail.com>
2023-06-13 18:02:05 +0700

    get rid of pm in plot_combo, more color, marker in evo

--- spectre/plot_function.py
+++ spectre/plot_function.py
@@ -16,6 +16,130 @@ from helper import yesno
 matplotlib.use("Agg")
 
 
+def plot_evo(result_solve_ivp, name, states, x_scale, more_species_mkm=None):
+    """"used in km_volcanic, mode0"""
+
+    r_indices = [i for i, s in enumerate(states) if s.lower().startswith("r")]
+    p_indices = [i for i, s in enumerate(states) if s.lower().startswith("p")]
+
+    if x_scale == "ls":
+        t = np.log10(result_solve_ivp.t)
+        xlabel = "log(time) (s)"
+    elif x_scale == "s":
+        t = result_solve_ivp.t
+        xlabel = "time (s)"
+    elif x_scale == "lmin":
+        t = np.log10(result_solve_ivp.t / 60)
+        xlabel = "log(time) (min)"
+    elif x_scale == "min":
+        t = result_solve_ivp.t / 60
+        xlabel = "time (min)"
+    elif x_scale == "h":
+        t = result_solve_ivp.t / 3600
+        xlabel = "time (h)"
+    elif x_scale == "d":
+        t = result_solve_ivp.t / 86400
+        xlabel = "time (d)"
+    else:
+        raise ValueError(
+            "x_scale must be 'ls', 's', 'lmin', 'min', 'h', or 'd'")
+
+    plt.rc("axes", labelsize=18)
+    plt.rc("xtick", labelsize=18)
+    plt.rc("ytick", labelsize=18)
+    plt.rc("font", size=18)
+
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(1, 1, 1)
+
+    # Catalyst---------
+    ax.plot(t,
+            result_solve_ivp.y[0, :],
+            c="#797979",
+            linewidth=2,
+            alpha=0.85,
+            zorder=1,
+            label=states[0])
+
+    # Reactant--------------------------
+    color_R = [
+        "#008F73",
+        "#1AC182",
+        "#1AC145",
+        "#7FFA35",
+        "#8FD810",
+        "#ACBD0A",
+        "#76B880",
+        "#195C0C"]
+
+    for n, i in enumerate(r_indices):
+        ax.plot(t,
+                result_solve_ivp.y[i, :],
+                linestyle="--",
+                c=color_R[n],
+                linewidth=2,
+                alpha=0.85,
+                zorder=1,
+                label=states[i])
+
+    # Product--------------------------
+    color_P = [
+        "#D80828",
+        "#F57D13",
+        "#55000A",
+        "#F34DD8",
+        "#C5A806",
+        "#602AFC",
+        "#156F93"
+        "#46597C"]
+
+    for n, i in enumerate(p_indices):
+        ax.plot(t,
+                result_solve_ivp.y[i, :],
+                linestyle="dashdot",
+                c=color_P[n],
+                linewidth=2,
+                alpha=0.85,
+                zorder=1,
+                label=states[i])
+
+    # additional INT-----------------
+    color_INT = [
+        "#4251B3",
+        "#3977BD",
+        "#2F7794",
+        "#7159EA",
+        "#15AE9B",
+        "#147F58"]
+    if more_species_mkm is not None:
+        for i in more_species_mkm:
+            ax.plot(t,
+                    result_solve_ivp.y[i, :],
+                    linestyle="dashdot",
+                    c=color_INT[i],
+                    linewidth=2,
+                    alpha=0.85,
+                    zorder=1,
+                    label=states[i])
+
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible
+    plt.xlabel(xlabel)
+    plt.ylabel('Concentration [mol/l]')
+    plt.legend(frameon=False)
+    plt.grid(True, linestyle='--', linewidth=0.75)
+    plt.tight_layout()
+
+    ymin, ymax = ax.get_ylim()
+    ybase = np.ceil(ymax) / 10
+    ymax = bround(ymax, ybase, type="max")
+    ymin = bround(ymin, ybase, type="min")
+    plt.ylim(ymin, ymax)
+    plt.yticks(np.arange(0, ymax + 0.1, ybase))
+
+    fig.savefig(f"kinetic_modelling_{name}.png", dpi=400)
+
+
 def plot_evo_save(
         result_solve_ivp,
         wdir,
@@ -69,7 +193,9 @@ def plot_evo_save(
         "#1AC145",
         "#7FFA35",
         "#8FD810",
-        "#ACBD0A"]
+        "#ACBD0A",
+        "#76B880",
+        "#195C0C"]
 
     for n, i in enumerate(r_indices):
         ax.plot(t,
@@ -88,7 +214,9 @@ def plot_evo_save(
         "#55000A",
         "#F34DD8",
         "#C5A806",
-        "#602AFC"]
+        "#602AFC",
+        "#156F93"
+        "#46597C"]
 
     for n, i in enumerate(p_indices):
         ax.plot(t,
@@ -249,25 +377,6 @@ def plotpoints(ax, px, py, cb, ms, plotmode):
         )
 
 
-def plotpoints_(ax, px, py, c, m, plotmode):
-    if plotmode == 1:
-        s = 30
-        lw = 0.3
-    else:
-        s = 15
-        lw = 0.25
-    ax.scatter(
-        px,
-        py,
-        s=s,
-        c=c,
-        marker=m,
-        linewidths=lw,
-        edgecolors="black",
-        zorder=2,
-    )
-
-
 def plot_2d_combo(
     x,
     y,
@@ -292,9 +401,11 @@ def plot_2d_combo(
         "#FF6347",
         "#32CD32",
         "#4169E1",
-        "#FFD700",
-        "#8A2BE2",
-        "#00FFFF"]
+        "#9607BC",
+        "#EA2CD3",
+        "#D88918",
+        "#148873",
+        "#000000"]
 
     marker = [
         "o",
@@ -302,7 +413,9 @@ def plot_2d_combo(
         "s",
         "P",
         "*",
-        "X"
+        "X",
+        "d",
+        "D"
     ]
     fig, ax = plt.subplots(
         frameon=False, figsize=[4.2, 3], dpi=300, constrained_layout=True,
@@ -315,11 +428,10 @@ def plot_2d_combo(
 
     # no scatter plot
     if plotmode == 0:
-
-        for i in range(y.shape[0]):
+        for i, yi in enumerate(y):
             ax.plot(
                 x,
-                y[i],
+                yi,
                 "-",
                 linewidth=1.5,
                 color=color[i],
@@ -339,88 +451,14 @@ def plot_2d_combo(
                         linewidth=0.75,
                         alpha=0.75,
                     )
+
     # mono color scatter plot
-    elif plotmode == 1:
+    elif plotmode > 1:
 
-        for i in range(y.shape[0]):
+        for i, yi in enumerate(y):
             ax.plot(
                 x,
-                y[i],
-                "-",
-                linewidth=1.5,
-                color=color[i],
-                alpha=0.95,
-                zorder=1,
-                label=labels[i])
-            ax = beautify_ax(ax)
-            if rid is not None and rb is not None:
-                avgs = []
-                rb.append(xmax)
-                for i in range(len(rb) - 1):
-                    avgs.append((rb[i] + rb[i + 1]) / 2)
-                for i in rb:
-                    ax.axvline(
-                        i,
-                        linestyle="dashed",
-                        color="black",
-                        linewidth=0.75,
-                        alpha=0.75,
-                        zorder=3,
-                    )
-            if ci[i] is not None:
-                plot_ci(ci[i], x, y[i], ax=ax)
-            plotpoints(ax, px, py[i], np.repeat(
-                [color[i]], len(px)), ms, plotmode)
-
-    elif plotmode == 2:
-        for i in range(y.shape[0]):
-            ax.plot(
-                x,
-                y[i],
-                "-",
-                linewidth=1.5,
-                color=color[i],
-                alpha=0.95,
-                zorder=1,
-                label=labels[i])
-            ax = beautify_ax(ax)
-            if rid is not None and rb is not None:
-                avgs = []
-                rb.append(xmax)
-                for i in range(len(rb) - 1):
-                    avgs.append((rb[i] + rb[i + 1]) / 2)
-                for i in rb:
-                    ax.axvline(
-                        i,
-                        linestyle="dashed",
-                        color="black",
-                        linewidth=0.5,
-                        alpha=0.75,
-                        zorder=3,
-                    )
-                yavg = (y[i].max() + y[i].min()) * 0.5
-                for i, j in zip(rid, avgs):
-                    plt.text(
-                        j,
-                        yavg,
-                        i,
-                        fontsize=7.5,
-                        horizontalalignment="center",
-                        verticalalignment="center",
-                        rotation="vertical",
-                        zorder=4,
-                    )
-            if ci[i] is not None:
-                plot_ci(ci[i], x, y[i], ax=ax)
-            plotpoints(ax, px, py[i], np.repeat(
-                [color[i]], len(px)), ms, plotmode)
-
-    # TODO color by the ranking
-    elif plotmode == 3 and y.shape[0] > 1:
-        for i in range(y.shape[0]):
-            ax.plot(
-                x,
-                y[i],
+                yi,
                 "-",
                 linewidth=1.5,
                 color=color[i],
@@ -454,125 +492,6 @@ def plot_2d_combo(
     plt.yticks(np.arange(ymin, ymax + 0.1, ybase))
     plt.legend(fontsize=10, loc='best')
     plt.savefig(filename)
-
-
-def plot_evo(result_solve_ivp, name, states, x_scale, more_species_mkm=None):
-
-    r_indices = [i for i, s in enumerate(states) if s.lower().startswith("r")]
-    p_indices = [i for i, s in enumerate(states) if s.lower().startswith("p")]
-
-    if x_scale == "ls":
-        t = np.log10(result_solve_ivp.t)
-        xlabel = "log(time) (s)"
-    elif x_scale == "s":
-        t = result_solve_ivp.t
-        xlabel = "time (s)"
-    elif x_scale == "lmin":
-        t = np.log10(result_solve_ivp.t / 60)
-        xlabel = "log(time) (min)"
-    elif x_scale == "min":
-        t = result_solve_ivp.t / 60
-        xlabel = "time (min)"
-    elif x_scale == "h":
-        t = result_solve_ivp.t / 3600
-        xlabel = "time (h)"
-    elif x_scale == "d":
-        t = result_solve_ivp.t / 86400
-        xlabel = "time (d)"
-    else:
-        raise ValueError(
-            "x_scale must be 'ls', 's', 'lmin', 'min', 'h', or 'd'")
-
-    plt.rc("axes", labelsize=18)
-    plt.rc("xtick", labelsize=18)
-    plt.rc("ytick", labelsize=18)
-    plt.rc("font", size=18)
-
-    fig = plt.figure(figsize=(8, 6))
-    ax = fig.add_subplot(1, 1, 1)
-
-    # Catalyst---------
-    ax.plot(t,
-            result_solve_ivp.y[0, :],
-            c="#797979",
-            linewidth=2,
-            alpha=0.85,
-            zorder=1,
-            label=states[0])
-
-    # Reactant--------------------------
-    color_R = [
-        "#008F73",
-        "#1AC182",
-        "#1AC145",
-        "#7FFA35",
-        "#8FD810",
-        "#ACBD0A"]
-
-    for n, i in enumerate(r_indices):
-        ax.plot(t,
-                result_solve_ivp.y[i, :],
-                linestyle="--",
-                c=color_R[n],
-                linewidth=2,
-                alpha=0.85,
-                zorder=1,
-                label=states[i])
-
-    # Product--------------------------
-    color_P = [
-        "#D80828",
-        "#F57D13",
-        "#55000A",
-        "#F34DD8",
-        "#C5A806",
-        "#602AFC"]
-
-    for n, i in enumerate(p_indices):
-        ax.plot(t,
-                result_solve_ivp.y[i, :],
-                linestyle="dashdot",
-                c=color_P[n],
-                linewidth=2,
-                alpha=0.85,
-                zorder=1,
-                label=states[i])
-
-    # additional INT-----------------
-    color_INT = [
-        "#4251B3",
-        "#3977BD",
-        "#2F7794",
-        "#7159EA",
-        "#15AE9B",
-        "#147F58"]
-    if more_species_mkm is not None:
-        for i in more_species_mkm:
-            ax.plot(t,
-                    result_solve_ivp.y[i, :],
-                    linestyle="dashdot",
-                    c=color_INT[i],
-                    linewidth=2,
-                    alpha=0.85,
-                    zorder=1,
-                    label=states[i])
-
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible
-    plt.xlabel(xlabel)
-    plt.ylabel('Concentration [mol/l]')
-    plt.legend(frameon=False)
-    plt.grid(True, linestyle='--', linewidth=0.75)
-    plt.tight_layout()
-
-    ymin, ymax = ax.get_ylim()
-    ybase = np.ceil(ymax) / 10
-    ymax = bround(ymax, ybase, type="max")
-    ymin = bround(ymin, ybase, type="min")
-    plt.ylim(ymin, ymax)
-    plt.yticks(np.arange(0, ymax + 0.1, ybase))
-
-    fig.savefig(f"kinetic_modelling_{name}.png", dpi=400)
 
 
 def plot_3d_(
