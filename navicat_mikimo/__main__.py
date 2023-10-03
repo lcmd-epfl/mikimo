57e2a522a81c3d698baa9de34d3673d0d5590f04
PregY <iflp1908sl@gmail.com>
2023-10-03 14:01:45 +0700

    doc before calling each mode

--- navicat_mikimo/__main__.py
+++ navicat_mikimo/__main__.py
@@ -6,6 +6,25 @@ from . import kinetic_solver, km_volcanic, screen_reaction_condition
 
 
 def run(mode):
+    """
+    Perform microkinetic modelling (mkm) for homogeneous reaction using energy data and the coulping with volcanic to perform mkm in a high-throughput fashion.
+
+    Run mikimo by the specified mode, which can be one of the following:
+    - 'mkm': Perform a single MKM run using the top-most row in the reaction data file.
+    - 'vp': Screen over all energy profiles in the reaction data file.
+    - 'cond': Screen over reaction time and/or temperature.
+
+    Usage:
+    - For 'mkm' mode: navicat_mikimo mkm
+    - For 'vp' mode: navicat_mikimo vp
+    - For 'cond' mode: navicat_mikimo cond
+
+    Args:
+        mode (str): The mode to run [Valid choices: 'mkm', 'vp', or 'cond'].
+
+    Returns:
+        None
+    """
     if mode == "mkm":
         sys.exit(kinetic_solver.main())
     elif mode == "vp":
